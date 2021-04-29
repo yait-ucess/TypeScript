3b912ff849f9928c3595aab8ff54c8c2c72f84e3
yait-ucess <bandai28@ezweb.ne.jp>
2021-04-29 20:41:54 +0900

    strictNullChecks strictFunctionTypes
    strictNullChecks
    厳しいnull型チェックです。
    
    strictFunctionTypes
    classの継承時に起こり得るバグを防ぐものです。

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,3 +1,8 @@
-function echo(message) {
+function echo(message: string) {
   return message;
 }
+
+let nullableMessage: string = null;
+let undefinedableMessage: string = undefined;
+let onlyNull: null = undefined;
+let onlyUndefined: undefined = null;
