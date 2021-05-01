d6ddef2033da4873c8b4674e058466a68f545912
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-01 16:59:03 +0900

    構造的部分型
    interface Human {
      name: string;
      age: number;
      greeting(message: string): void;
    }
    
    const user: Human = tmpDeveloper;のように、userは「少なくともHumanの中身の要素は持っているんだ」と分かるのが良いことです。

--- src/interface.ts
+++ src/interface.ts
@@ -5,8 +5,18 @@ interface Human {
 }
 
 class Developer implements Human{
-  constructor(public name: string, public age: number) {}
+  constructor(public name: string, public age: number, public experience: number) {}
   greeting(message: string) {
-    console.log('Hello');
+    console.log(message);
   }
 }
+
+const tmpDeveloper = {
+  name: 'Quill',
+  age: 38,
+  experience: 3,
+  greeting(message: string) {
+    console.log(message);
+  }
+}
+const user: Human = tmpDeveloper;
