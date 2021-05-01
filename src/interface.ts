03944fa60c37c6ef3404188de5adfaba8735f17e
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-01 16:00:33 +0900

    Interfaceにメソッドを追加する
    greeting: (message: string) => void;
    と
    greeting(message: string): void;

--- src/interface.ts
+++ src/interface.ts
@@ -1,11 +1,15 @@
 interface Human {
   name: string;
   age: number;
+  greeting(message: string): void;
 }
 
-const human: Human = {
+const human = {
   name: 'Quill',
-  age: 38
+  age: 38,
+  greeting(message: string): void {
+    console.log(message);
+  }
 }
 
 let developer: Human;
