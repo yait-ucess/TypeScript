3c0bdab554084710724dfc7065e3fcc96e4629d3
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-03 15:58:09 +0900

    Interfaceに対してgenericsを使う方法
    interfaceを定義した名前の後に、＜T＞のように付けることができます。
    Tの型はconst tmpDataBase: TmpDataBase<number>のように使うことができます。
    
    interface TmpDataBase<T> {
      id: number;
      data: T[];
    }
    
    const tmpDataBase: TmpDataBase<number> = {
      id: 3,
      data: [20]
    }

--- src/generics.ts
+++ src/generics.ts
@@ -1,25 +1,35 @@
-function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
-  value[key];
-  return value;
-}
-console.log(copy({ name: 'Quill', age: 20}, 'age'));
-type K = keyof { name: {}; number: number}
+// function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+//   value[key];
+//   return value;
+// }
+// console.log(copy({ name: 'Quill', age: 20}, 'age'));
+// type K = keyof { name: {}; number: number}
 
-class LightDataBase<T extends string | number | boolean> {
-  private data: T[] = [];
-  add(item: T) {
-    this.data.push(item);
-  }
-  remove(item: T) {
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-  get() {
-    return this.data;
-  }
+// class LightDataBase<T extends string | number | boolean> {
+//   private data: T[] = [];
+//   add(item: T) {
+//     this.data.push(item);
+//   }
+//   remove(item: T) {
+//     this.data.splice(this.data.indexOf(item), 1);
+//   }
+//   get() {
+//     return this.data;
+//   }
+// }
+// const stringLightDataBase = new LightDataBase<string>();
+// stringLightDataBase.add('Apple');
+// stringLightDataBase.add('Banana');
+// stringLightDataBase.add('Orange');
+// stringLightDataBase.remove('Apple');
+// console.log(stringLightDataBase.get());
+
+interface TmpDataBase<T> {
+  id: number;
+  data: T[];
 }
-const stringLightDataBase = new LightDataBase<string>();
-stringLightDataBase.add('Apple');
-stringLightDataBase.add('Banana');
-stringLightDataBase.add('Orange');
-stringLightDataBase.remove('Apple');
-console.log(stringLightDataBase.get());
+
+const tmpDataBase: TmpDataBase<number> = {
+  id: 3,
+  data: [20]
+}
