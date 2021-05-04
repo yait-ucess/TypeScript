4d5ab3b502a2b3523ccee1220d284c935c339afa
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-04 18:46:39 +0900

    複数のDecoratorを扱う際の順番
    Decoratorは下から上に実行されます。
    Decorator Factoriesは上から下に実行されます。

--- dist/decorator.js
+++ dist/decorator.js
@@ -4,9 +4,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logging(constructor) {
-    console.log('Logging...');
-    console.log(constructor);
+function Logging(message) {
+    console.log('Logging Factory');
+    return function (constructor) {
+        console.log(message);
+        console.log(constructor);
+    };
+}
+function Component(template, selector) {
+    console.log('Component Factory');
+    return function (constructor) {
+        const mountedElement = document.querySelector(selector);
+        console.log('Component');
+        const instance = new constructor();
+        if (mountedElement) {
+            mountedElement.innerHTML = template;
+            mountedElement.querySelector('h1').textContent = instance.name;
+        }
+    };
 }
 let User = class User {
     constructor() {
@@ -15,5 +30,6 @@ let User = class User {
     }
 };
 User = __decorate([
-    Logging
+    Logging('Logging User'),
+    Component('<h1>{{ name }}</h1>', '#app')
 ], User);
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,4 +1,5 @@
 function Logging(message: string) {
+  console.log('Logging Factory');
   return function (constructor: Function) {
     console.log(message);
     console.log(constructor);
@@ -6,8 +7,10 @@ function Logging(message: string) {
 }
 
 function Component(template: string, selector: string) {
+  console.log('Component Factory');
   return function(constructor: { new(): { name: string } }) {
     const mountedElement = document.querySelector(selector);
+    console.log('Component');
     const instance = new constructor();
     if (mountedElement) {
       mountedElement.innerHTML = template;
@@ -16,8 +19,8 @@ function Component(template: string, selector: string) {
   }
 }
 
-@Component('<h1>{{ name }}</h1>', '#app')
 @Logging('Logging User')
+@Component('<h1>{{ name }}</h1>', '#app')
 class User {
   name = 'Quill';
   constructor() {
