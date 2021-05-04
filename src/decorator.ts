e0bcf240bcdfa70076523eca519bb865bc8eab06
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-04 17:32:41 +0900

    Dacoratorの応用的な使い方を試してみる
    @Component('<h1>{{ name }}</h1>', '#app')

--- src/decorator.ts
+++ src/decorator.ts
@@ -5,6 +5,18 @@ function Logging(message: string) {
   }  
 }
 
+function Component(template: string, selector: string) {
+  return function(constructor: { new(): { name: string } }) {
+    const mountedElement = document.querySelector(selector);
+    const instance = new constructor();
+    if (mountedElement) {
+      mountedElement.innerHTML = template;
+      mountedElement.querySelector('h1')!.textContent = instance.name;
+    }
+  }
+}
+
+@Component('<h1>{{ name }}</h1>', '#app')
 @Logging('Logging User')
 class User {
   name = 'Quill';
