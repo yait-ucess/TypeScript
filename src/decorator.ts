f509cc5d359ba38186e60c2bbfa82b3230398046
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-05 11:09:08 +0900

    liquidation
    不要なファイルを整理

---
+++ src/decorator.ts
@@ -0,0 +1,83 @@
+function Logging(message: string) {
+  return function (constructor: Function) {
+    console.log(message);
+    console.log(constructor);
+  }  
+}
+
+function Component(template: string, selector: string) {
+  console.log('Component Factory');
+  return function <T extends { new(...args: any[]): { name: string } } >(constructor: T) {
+    
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        const mountedElement = document.querySelector(selector);
+        const instance = new constructor();
+        if (mountedElement) {
+          mountedElement.innerHTML = template;
+          mountedElement.querySelector('h1')!.textContent = instance.name;
+        }
+      }
+    }
+  }
+}
+
+console.dir(Logging);
+function PropertyLogging(target: any, propertyKey: string) {
+  console.log('PropertyLogging');
+  console.log(target);
+  console.log(propertyKey);
+}
+
+function MethodLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log('MethodLogging');
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+}
+
+function enumerable(isEnumerable: boolean) {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return {
+      enumerable: isEnumerable
+    }
+  }
+}
+
+function AccessorLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log('AccessorLogging');
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+}
+
+function ParameterLogging(target: any, propertyKey: string, parameterIndex: number) {
+  console.log('ParameterLogging');
+  console.log(target);
+  console.log(propertyKey);
+  console.log(parameterIndex);
+}
+
+@Logging('Logging User')
+@Component('<h1>{{ name }}</h1>', '#app')
+class User {
+  @PropertyLogging
+  name = 'Quill';
+  constructor(private _age: number) {
+    console.log('User was created!');
+  }
+  @AccessorLogging
+  get age() {
+    return this._age;
+  }
+  set age(value) {
+    this._age = value;
+  }
+  @enumerable(false)
+  @MethodLogging
+  greeting( @ParameterLogging message: string) {
+    console.log(message);
+  }
+}
+
