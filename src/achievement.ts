3ca589924c79b3c264832c75ff489550a82150b1
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-05 14:08:27 +0900

    AchieveListに、全てのachieveの要素を保持させる。
    querySelectorAllで取ってくる

---
+++ src/achievement.ts
@@ -0,0 +1,18 @@
+class Score {
+
+}
+
+class Achieve {
+
+}
+
+class AchieveList {
+  elements = document.querySelectorAll<HTMLDivElement>('.achieve');
+  constructor() {
+    this.elements.forEach(element => {
+
+    })
+  }
+}
+
+const achieveList = new AchieveList();
