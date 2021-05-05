13e260a4102e641450db80160e7b30a0abf18645
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-05 10:20:10 +0900

    Accessor Decorators
    Method Decoratorsと一緒です。

--- dist/decorator.js
+++ dist/decorator.js
@@ -38,11 +38,23 @@ function MethodLogging(target, propertyKey, descriptor) {
     console.log(propertyKey);
     console.log(descriptor);
 }
+function AccessorLogging(target, propertyKey, descriptor) {
+    console.log('AccessorLogging');
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+}
 let User = class User {
-    constructor(age) {
-        this.age = age;
+    constructor(_age) {
+        this._age = _age;
         this.name = 'Quill';
         console.log('User was created!');
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
     }
     greeting() {
         console.log('Hello');
@@ -52,10 +64,12 @@ __decorate([
     PropertyLogging
 ], User.prototype, "name", void 0);
 __decorate([
+    AccessorLogging
+], User.prototype, "age", null);
+__decorate([
     MethodLogging
 ], User.prototype, "greeting", null);
 User = __decorate([
     Logging('Logging User'),
     Component('<h1>{{ name }}</h1>', '#app')
 ], User);
-const user1 = new User(20);
--- src/decorator.ts
+++ src/decorator.ts
@@ -37,13 +37,27 @@ function MethodLogging(target: any, propertyKey: string, descriptor: PropertyDes
   console.log(descriptor);
 }
 
+function AccessorLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log('AccessorLogging');
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+}
+
 @Logging('Logging User')
 @Component('<h1>{{ name }}</h1>', '#app')
 class User {
   @PropertyLogging
   name = 'Quill';
-  constructor(public age: number) {
+  constructor(private _age: number) {
     console.log('User was created!');
+  }
+  @AccessorLogging
+  get age() {
+    return this._age;
+  }
+  set age(value) {
+    this._age = value;
   }
   @MethodLogging
   greeting() {
@@ -51,4 +65,3 @@ class User {
   }
 }
 
-const user1 = new User(20);
