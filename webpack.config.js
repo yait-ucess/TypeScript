7f01435f181ef7774158733d99d3f07e59e55c98
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-07 20:09:31 +0900

    setup webpack
    ①　webpack専用のconfigファイル（webpack.config.js）を作成します。
    ②  node.jsのexport文を書きます。
    ③  entryを設定します。
    ④  outputを設定します。

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
 module.exports = {
-  entry: './dist/main.js'
+  entry: './dist/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
+
+console.log(path.resolve(__dirname, 'dist'))
