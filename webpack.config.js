979960254d679a03ad3a6b6f6fe15a0dc3310b05
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-08 11:23:35 +0900

    webpack-dev-server
    webpackを使用したフロントエンド開発時に利用できる開発環境向けのwebサーバーです。

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/achievement/main.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/d'
   },
   devtool: 'inline-source-map',
   module: {
