8677bcaf1604e36e363f687873425cd92bec5ac2
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-10 14:16:02 +0900

    namespace
    名前空間です。名前の衝突を防ぐ役割があります。
    しかし、import, exportを優先するように言われており、あまり使う機会はありません。
    
    〜.〜のような型があれば、基本的にはnamespaceが使われていると思ってください。

--- src/library.ts
+++ src/library.ts
@@ -2,5 +2,13 @@
 // axios.get('');
 
 // import _ from 'lodash';
-console.log(_.shuffle([1, 2, 3, 4]))
+//console.log(_.shuffle([1, 2, 3, 4]))
+namespace myApp {
+  const hello = 'hello in namespace';
+  export const name = 'Quill';
+  export interface Nameable {
+    name: string;
+  }
+}
 
+let nameable: myApp.Nameable;
--- src/lodash.d.ts
+++ src/lodash.d.ts
@@ -2,8 +2,6 @@ declare module 'lodash' {
   export function shuffle<T>(arr:  T[]): T[]
 }
 
-interface Lodash {
-  shuffle<T>(arr:  T[]): T[]
+declare namespace _ {
+  function shuffle<T>(arr:  T[]): T[]
 }
-
-declare const _: Lodash
