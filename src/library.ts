7ace61112dbb4c4e5d2cb340f77c25b6c743047a
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-11 10:58:30 +0900

    lodash型定義ファイルの中身
    /// <reference path="./common/common.d.ts" />
    ↑ 「トリプルスラッシュディレクティブ」
    ↑ ts → jsに変換したときに無くなります。
    
    export = _;
    ↑ node.js用のexport文。
    
    export as namespace _;
    ↑ importを使わずに、直接読み込みなどをしたときに、_を使えるようにするための決まり文句。

--- src/library.ts
+++ src/library.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-// axios.get('');
-
-// import _ from 'lodash';
-//console.log(_.shuffle([1, 2, 3, 4]))
+import _ from 'lodash';
+axios.get('https://fooapi.com');
+console.log(_.shuffle([1, 2, 3, 4]))
 namespace myApp {
   const hello = 'hello in namespace';
   export const name = 'Quill';
--- src/lodash.d.ts
+++ src/lodash.d.ts
@@ -1,7 +1,7 @@
-declare module 'lodash' {
-  export function shuffle<T>(arr:  T[]): T[]
-}
+// declare module 'lodash' {
+//   export function shuffle<T>(arr:  T[]): T[]
+// }
 
-declare namespace _ {
-  function shuffle<T>(arr:  T[]): T[]
-}
+// declare namespace _ {
+//   function shuffle<T>(arr:  T[]): T[]
+// }
