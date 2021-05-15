3234e476a7e4b3dcb09d930e4c3430ac8595402e
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-15 12:00:27 +0900

    ゼロからTypeScriptとReactを一緒に使う
    commit参照

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', 
-  entry: './src/library.ts',
+  entry: './src/react.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,12 +11,12 @@ module.exports = {
   devtool: 'inline-source-map',
   module: {
     rules: [{
-      test: /\.ts$/,
+      test: /\.tsx?$/,
       use: 'ts-loader',
       exclude: /node_modules/
     }]
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js']
   }
 }
