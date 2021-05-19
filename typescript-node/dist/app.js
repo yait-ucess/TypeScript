a36733266856b25ab9c5c18a0932d99718eab24e
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-19 20:49:10 +0900

    エラーハンドリング
    エラーハンドリングです。
    ↓どこかでエラーが見つかれば、Expressが認識して、
    ↓この関数にエラーが集まって、関数内の処理が実行されます。
    
    app.use((err, req, res, next) => {
      next();
    })

--- typescript-node/dist/app.js
+++ typescript-node/dist/app.js
@@ -1,6 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// import * as express from 'express';
-const express = require("express");
-const app = express();
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use((req, res, next) => {
+    next();
+});
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello</h1>');
+});
+app.use((err, req, res, next) => {
+    next();
+});
 app.listen(3000);
--- typescript-node/src2/app.ts
+++ typescript-node/src2/app.ts
@@ -1,4 +1,12 @@
-// import * as express from 'express';
-import express = require('express');
+import express from 'express';
 const app = express();
+app.use((req, res, next) => {
+  next();
+})
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello</h1>')
+})
+app.use((err, req, res, next) => {
+  next();
+})
 app.listen(3000);
