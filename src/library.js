29e4102cd871f99557e877bb5010cbadb8649379
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-19 13:16:34 +0900

    nodemonインストール
    nodemonとは、ソースコードの変更を監視し、自動的にnodeコマンドを再起動してくれるnpmパッケージです。
    
    npm install --save-dev nodemon

--- src/library.js
+++ src/library.js
@@ -1,10 +1,27 @@
-// import axios from 'axios';
-// axios.get('');
-// import _ from 'lodash';
-//console.log(_.shuffle([1, 2, 3, 4]))
+import axios from 'axios';
+import _ from 'lodash';
+axios.get('https://fooapi.com');
+console.log(_.shuffle([1, 2, 3, 4]));
 var myApp;
 (function (myApp) {
-    var hello = 'hello in namespace';
+    const hello = 'hello in namespace';
     myApp.name = 'Quill';
 })(myApp || (myApp = {}));
-var hello = myApp.name;
+let nameable;
+hello;
+//let name: string;
+//function name() {}
+//enum name {}
+// class name {}
+// interface name {
+//   first: string
+// }
+// interface name {
+//   second: number
+// }
+// namespace name{
+//   const first: string = 'Peter';
+// }
+// namespace name{
+//   const first: string = 'Peter';
+// }
