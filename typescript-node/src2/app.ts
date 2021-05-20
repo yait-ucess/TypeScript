77713b1987375564271affb83c7732ac6e7bf2c9
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-20 11:21:58 +0900

    expressでbodyを使う方法
    app.use(express.json());があることで
    
    app.post('/', (req, res, next) => {
      res.send(`<h1>I got ${req.body}</h1>`)
    })
    
    のように、bodyでデータを受け取ることができる。

--- typescript-node/src2/app.ts
+++ typescript-node/src2/app.ts
@@ -1,10 +1,14 @@
 import express { Request, Response, NextFunction  } from 'express';
 const app = express();
+app.use(express.json());
 app.use((req, res, next) => {
   next();
 })
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello</h1>')
+})
+app.post('/', (req, res, next) => {
+  res.send(`<h1>I got ${req.body}</h1>`)
 })
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   next();
