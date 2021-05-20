3a8f419a11de9f7eac0c60e8dfffd883b0fbbd70
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-20 11:36:43 +0900

    postman
    API開発のためのコラボレーションプラットフォーム
    API構築の各ステップを簡素化し、コラボレーションを合理化して、より優れたAPIをより迅速に作成できるようにします。

--- typescript-node/dist/app.js
+++ typescript-node/dist/app.js
@@ -1,16 +1,20 @@
 "use strict";
-var express = ;
-{
-    Request, Response, NextFunction;
-}
-from;
-'express';
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use(express_1.default.json());
 app.use((req, res, next) => {
     next();
 });
 app.get('/', (req, res, next) => {
     res.send('<h1>Hello</h1>');
+});
+app.post('/', (req, res, next) => {
+    console.log(req.body.message);
+    res.send(`<h1>I got ${req.body.message}</h1>`);
 });
 app.use((err, req, res, next) => {
     next();
--- typescript-node/src2/app.ts
+++ typescript-node/src2/app.ts
@@ -1,4 +1,9 @@
-import express { Request, Response, NextFunction  } from 'express';
+import express, { Request, Response, NextFunction  } from 'express';
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  }
+}
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
@@ -7,8 +12,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello</h1>')
 })
-app.post('/', (req, res, next) => {
-  res.send(`<h1>I got ${req.body}</h1>`)
+app.post('/', (req: MessageRequest, res, next) => {
+  console.log(req.body.message)
+  res.send(`<h1>I got ${req.body.message}</h1>`)
 })
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   next();
